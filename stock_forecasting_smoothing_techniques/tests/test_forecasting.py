8a940f145db6fdbb0b06e87a03d0f03699f7427c
Katherine Zaoral <kathy@vauxoo.com>
2015-08-13 09:44:35 -0430

    [IMP] Update tests to work with the last version of code. Also add a new sheet file that holds the data of the test data, with all the formulas and posible results.
    Detail changes:
    - [ADD] transform expected and real value to int and then compare.
    - [TODO] the last verification can be imporve using numpy.
    - [ADD] call calculate forecast in every test.
    - [ADD] update out values of every test method.
    - [REM] delete commented and noy used test_all method.
    - [ADD] split test 03 (holt) for diffrente k period in future.
    - [ADD] add new test 4 that check SMA, CMA and WMA.
    - [ADD] add new test 5 that check SMA and CMA.

--- stock_forecasting_smoothing_techniques/tests/test_forecasting.py
+++ stock_forecasting_smoothing_techniques/tests/test_forecasting.py
@@ -37,10 +37,10 @@ class TestForecasting(common.TransactionCase):
         error_msg = "{key:15} {real:15} != {expected:15} {ca} {diff:15}"
         for key in keys:
             vreal = real.get(key)
-            vexpected  = expected.get(key)
-            vdiff = abs(vreal - vexpected)
-            allowed_error = 0.1
-            if vdiff > allowed_error:
+            vexpected = expected.get(key)
+            vdiff = abs(vreal - float(vexpected))
+            allowed_error = 2
+            if int(round(vdiff)) > allowed_error:
                 elist += [error_msg.format(
                     key=key,
                     real=vreal,
@@ -57,6 +57,7 @@ class TestForecasting(common.TransactionCase):
         """
         values = self.get_test_01_in()
         forecast = self.forecast_obj.create(values)
+        forecast.calculate()
         out = self.get_test_01_out()
         self.compare(out, forecast.read(out.keys())[0])
 
@@ -80,27 +81,20 @@ class TestForecasting(common.TransactionCase):
         Return a dictionary with the expected result of the test.
         """
         return dict(
-            ma_forecast=14.0,
-            ma_ma_error=3.157895,
-            wma_forecast=4.666667,
-            wma_ma_error=10.684211,
-            single_forecast=17.690605,
-            single_ma_error=2.881943,
-            double_forecast=19.755723,
-            double_ma_error=5.155758,
-            triple_forecast=19.580177,
-            triple_ma_error=6.901317,
-            holt_forecast=18.221501,
-            holt_ma_error=3.003755
+            cma_forecast=18.0, cma_ma_error=3.15,
+            sma_forecast=17.0, sma_ma_error=4.6,
+            wma_forecast=18.666667, wma_ma_error=2.10,
+            single_forecast=17.690605, single_ma_error=2.881943,
+            double_forecast=19.755723, double_ma_error=5.155758,
+            triple_forecast=19.580177, triple_ma_error=6.901317,
+            holt_forecast=18.221501, holt_ma_error=3.003755
         )
 
     def test_02(self):
-        """
-        Run 10 values
-        - Only 10 forecast values of the 80 spaces.
-        """
+        """ All Table with 10 values """
         values = self.get_test_02_in()
         forecast = self.forecast_obj.create(values)
+        forecast.calculate()
         out = self.get_test_02_out()
         self.compare(out, forecast.read(out.keys())[0])
 
@@ -121,33 +115,19 @@ class TestForecasting(common.TransactionCase):
         Return a dictionary with the expected result of the test.
         """
         return dict(
-            ma_forecast=4.0,
-            ma_ma_error=2,
-            wma_forecast=1.333333,
-            wma_ma_error=5.666667,
-            single_forecast=7.760825,
-            single_ma_error=1.577526,
-            double_forecast=9.481168,
-            double_ma_error=2.691897,
-            triple_forecast=9.451321,
-            triple_ma_error=3.432094,
-            holt_forecast=11,
-            holt_ma_error=0
+            sma_forecast=7.0, sma_ma_error=3.0,
+            cma_forecast=8.0, cma_ma_error=2.0,
+            wma_forecast=8.66666, wma_ma_error=1.33,
+            single_forecast=7.760825, single_ma_error=1.577526,
+            double_forecast=9.481168, double_ma_error=2.691897,
+            triple_forecast=9.451321, triple_ma_error=3.432094,
+            holt_forecast=11, holt_ma_error=0
         )
-
-    # def _test_all(self):
-    #     """
-    #     Run all the test cases know.
-    #     """
-    #     for test_num in range(1, 5):
-    #         test_name = 'test_{num:02d}'.format(num=test_num)
-    #         values, out = self.get_test_data(test_name)
-    #         forecast = self.forecast_obj.create(values)
-    #         self.compare(out, forecast.read([])[0])
 
     def get_test_data(self, test_name):
         """
         return tupla in, out with the values to use in the test.
+              values, out = self.get_test_data(test_name)
         """
         data = {
             'test_01': self.get_test_01_data(),
@@ -179,8 +159,8 @@ class TestForecasting(common.TransactionCase):
                 val += 1
 
         out = dict(
-            ma_forecast=14.0,
-            ma_ma_error=3.157895,
+            cma_forecast=14.0,
+            cma_ma_error=3.157895,
             wma_forecast=4.666667,
             wma_ma_error=10.684211,
             single_forecast=17.690605,
@@ -208,8 +188,8 @@ class TestForecasting(common.TransactionCase):
             values.update({fvfield: val})
             val += 1
         out = dict(
-            ma_forecast=4.0,
-            ma_ma_error=2,
+            cma_forecast=4.0,
+            cma_ma_error=2,
             wma_forecast=1.333333,
             wma_ma_error=5.666667,
             single_forecast=7.760825,
@@ -223,15 +203,30 @@ class TestForecasting(common.TransactionCase):
         )
         return {'in': values, 'out': out}
 
-    def test_03(self):
-        """
-        Run 10 values
-        - Only 10 forecast values of the 80 spaces.
-        """
+    def _test_03(self, period):
+        """ base for various periods """
         values = self.get_test_03_in()
+        values.update(holt_period=period)
         forecast = self.forecast_obj.create(values)
-        out = self.get_test_03_out()
+        forecast.calculate()
+        out = self.get_test_03_out(period)
         self.compare(out, forecast.read(out.keys())[0])
+
+    def test_03_1(self):
+        """ holt(k=1) for 11 values """
+        self._test_03(1)
+
+    def test_03_2(self):
+        """ holt(k=2) for 11 values """
+        self._test_03(2)
+
+    def test_03_3(self):
+        """ holt(k=3) for 11 values """
+        self._test_03(3)
+
+    def test_03_4(self):
+        """ holt(k=4) for 11 values """
+        self._test_03(4)
 
     def get_test_03_in(self):
         """
@@ -251,22 +246,44 @@ class TestForecasting(common.TransactionCase):
             'fv_11': 343,
             'holt_alpha': 0.7,
             'beta': 0.6,
-            'period': 2,
         }
         return data
 
-    def get_test_03_out(self):
+    def get_test_03_out(self, period=1):
         """
-        Return a dictionary with the expected result of the test.
+        Return a list with the result values.
         """
-        # period forecast
-        # 1	359.7
-        # 2	372.6
-        # 3	385.4
-        # 4	398.3
-        return dict(
-            holt_forecast=372.6,
-        )
+        out = dict()
+        if period == 1:
+            out = dict(
+                holt_period=1, holt_forecast=359.70, holt_ma_error=17.67,
+                sma_forecast=292.60, sma_ma_error=76.4,
+                cma_forecast=315.00, cma_ma_error=48.2,
+            )
+        elif period == 2:
+            out = dict(
+                holt_period=2, holt_forecast=372.6,
+            )
+        elif period == 3:
+            out = dict(
+                holt_period=3, holt_forecast=385.4,
+            )
+        elif period == 4:
+            out = dict(
+                holt_period=4, holt_forecast=398.3,
+            )
+        else:
+            raise Exception(
+                _('There is not output for the enter count of periods'))
+        return out
+
+    def test_04(self):
+        """ SMA(5) and WMA(5) in 12 integer values """
+        values = self.get_test_04_in()
+        forecast = self.forecast_obj.create(values)
+        forecast.calculate()
+        out = self.get_test_04_out()
+        self.compare(out, forecast.read(out.keys())[0])
 
     def get_test_04_in(self):
         """
@@ -291,22 +308,48 @@ class TestForecasting(common.TransactionCase):
     def get_test_04_out(self):
         """
         Return a dictionary with the expected result of the test.
-
-        Week	Sales ($1000)	MA(5)	WMA(5)
-        1	    105	            -	    -
-        2	    100	            -	    -
-        3	    105	            -	    -
-        4	    95	            -	    -
-        5	    100	            101	    100
-        6	    95	            99	    98
-        7	    105	            100	    100
-        8	    120	            103	    107
-        9	    115	            107	    111
-        10	    125	            117	    116
-        11	    120	            120	    119
-        12	    120	            120	    119
         """
         return dict(
-            ma=120,
-            wma=119,
+            sma_forecast=117.00, sma_ma_error=10.42,
+            cma_forecast=120.00, cma_ma_error=6.37,
+            wma_forecast=120.33, wma_ma_error=4.37,
+        )
+
+    def test_05(self):
+        """ SMA(3) in 12 integer values """
+        values = self.get_test_05_in()
+        forecast = self.forecast_obj.create(values)
+        forecast.calculate()
+        self.assertEqual(forecast.period, 3)
+        out = self.get_test_05_out()
+        self.compare(out, forecast.read(out.keys())[0])
+
+    def get_test_05_in(self):
+        """
+        This are data to run the forecast test_05
+        """
+        data = {
+            'fv_01': 80,
+            'fv_02': 90,
+            'fv_03': 85,
+            'fv_04': 70,
+            'fv_05': 80,
+            'fv_06': 105,
+            'fv_07': 100,
+            'fv_08': 105,
+            'fv_09': 100,
+            'fv_10': 105,
+            'fv_11': 100,
+            'fv_12': 150,
+            'period': 3,
+        }
+        return data
+
+    def get_test_05_out(self):
+        """
+        Return a dictionary with the expected result of the test.
+        """
+        return dict(
+            sma_forecast=101.66, sma_ma_error=14.07,
+            cma_forecast=118.33, cma_ma_error=7.9,
         )
