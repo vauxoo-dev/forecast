34a203773616796291d2fa293181a07c18a276ee
Katherine Zaoral <kathy@vauxoo.com>
2015-08-17 13:41:28 -0430

    [IMP] update test to work with the new changes in the model> - [REM] The forecast.caculate is not required anymore now that the fields are   compute. - [IMP] The claer test section only check to clear the forecast values. - The get_test_0x_in methods were delete an now use the demo data to run the   test. Remeber, in this case, the test is only a check of the resulting   values.

--- stock_forecasting_smoothing_techniques/tests/test_forecasting.py
+++ stock_forecasting_smoothing_techniques/tests/test_forecasting.py
@@ -58,7 +58,6 @@ class TestForecasting(common.TransactionCase):
 
         # Test Create
         forecast = self.forecast_obj.create({})
-        forecast.calculate()
 
         # Test Read + Check defaults values
         defaults = {
@@ -120,20 +119,17 @@ class TestForecasting(common.TransactionCase):
 
     def test_00_1(self):
         """ Check Clear method/button works propertly """
-        sections = [
-            'all', 'sma', 'cma', 'wma', 'single', 'double', 'triple',
-            'holt',
-        ]
+        sections = ['data']
         for section in sections:
-            values = self.get_test_01_in()
-            forecast = self.forecast_obj.create(values)
-            forecast.calculate()
+            forecast0 = self.forecast_obj.browse(self.ref(
+                'stock_forecasting_smoothing_techniques.fst_demo_01'))
+            forecast = forecast0.copy()
             self._clear_section_test(forecast, section)
 
     def _clear_section_test(self, forecast, dtype):
         """ Clear a specific section onyl clear the section """
         # Prepare compare dictionaries
-        field_to_clear = forecast._fields_to_clear().get(dtype)
+        field_to_clear = forecast.fields_section(dtype)
         forecast_full = forecast.read(field_to_clear)[0]
         forecast_clean = forecast_full.copy()
         forecast_clean.update({}.fromkeys(field_to_clear, 0.0))
@@ -147,26 +143,10 @@ class TestForecasting(common.TransactionCase):
         """
         Run 80 values, count for 1 to 30 and repeat.
         """
-        values = self.get_test_01_in()
-        forecast = self.forecast_obj.create(values)
-        forecast.calculate()
+        forecast = self.forecast_obj.browse(self.ref(
+            'stock_forecasting_smoothing_techniques.fst_demo_01'))
         out = self.get_test_01_out()
         self.compare(out, forecast.read(out.keys())[0])
-
-    def get_test_01_in(self):
-        """
-        config the values for the forecasting model to test.
-        """
-        data = {}
-        val = 1
-        for item in range(1, 81):
-            fvfield = 'fv_{num:02d}'.format(num=item)
-            data.update({fvfield: val})
-            if val == 30:
-                val = 1
-            else:
-                val += 1
-        return data
 
     def get_test_01_out(self):
         """
@@ -184,23 +164,10 @@ class TestForecasting(common.TransactionCase):
 
     def test_02(self):
         """ All Table with 10 values """
-        values = self.get_test_02_in()
-        forecast = self.forecast_obj.create(values)
-        forecast.calculate()
+        forecast = self.forecast_obj.browse(self.ref(
+            'stock_forecasting_smoothing_techniques.fst_demo_02'))
         out = self.get_test_02_out()
         self.compare(out, forecast.read(out.keys())[0])
-
-    def get_test_02_in(self):
-        """
-        This method will return the forecast input values in a list.
-        """
-        data = {}
-        val = 1
-        for item in range(1, 11):
-            fvfield = 'fv_{num:02d}'.format(num=item)
-            data.update({fvfield: val})
-            val += 1
-        return data
 
     def get_test_02_out(self):
         """
@@ -216,90 +183,11 @@ class TestForecasting(common.TransactionCase):
             holt_forecast=11, holt_ma_error=0
         )
 
-    def get_test_data(self, test_name):
-        """
-        return tupla in, out with the values to use in the test.
-              values, out = self.get_test_data(test_name)
-        """
-        data = {
-            'test_01': self.get_test_01_data(),
-            'test_02': self.get_test_02_data(),
-            'test_03': {'in': self.get_test_03_in(),
-                        'out': self.get_test_03_out(),
-                        },
-            'test_04': {'in': self.get_test_04_in(),
-                        'out': self.get_test_04_out(),
-                        },
-        }
-        test_data = data.get(test_name)
-        return test_data.get('in'), test_data.get('out')
-
-    def get_test_01_data(self):
-        """
-        return dictionary with the keys (in, out).
-        - in: values to create the forecast record. Used to config the test.
-        - out: the expected results of the test.
-        """
-        values = {}
-        val = 1
-        for item in range(1, 81):
-            fvfield = 'fv_{num:02d}'.format(num=item)
-            values.update({fvfield: val})
-            if val == 30:
-                val = 1
-            else:
-                val += 1
-
-        out = dict(
-            cma_forecast=14.0,
-            cma_ma_error=3.157895,
-            wma_forecast=4.666667,
-            wma_ma_error=10.684211,
-            single_forecast=17.690605,
-            single_ma_error=2.881943,
-            double_forecast=19.755723,
-            double_ma_error=5.155758,
-            triple_forecast=19.580177,
-            triple_ma_error=6.901317,
-            holt_forecast=18.221501,
-            holt_ma_error=3.003755
-        )
-        return {'in': values, 'out': out}
-
-    def get_test_02_data(self):
-        """
-        return dictionary with the keys (in, out).
-        - in: values to create the forecast record. Used to config the test.
-        - out: the expected results of the test.
-        """
-        values = {}
-        val = 1
-        for item in range(1, 11):
-            fvfield = 'fv_{num:02d}'.format(num=item)
-            values.update({fvfield: val})
-            val += 1
-        out = dict(
-            cma_forecast=4.0,
-            cma_ma_error=2,
-            wma_forecast=1.333333,
-            wma_ma_error=5.666667,
-            single_forecast=7.760825,
-            single_ma_error=1.577526,
-            double_forecast=9.481168,
-            double_ma_error=2.691897,
-            triple_forecast=9.451321,
-            triple_ma_error=3.432094,
-            holt_forecast=11,
-            holt_ma_error=0
-        )
-        return {'in': values, 'out': out}
-
     def _test_03(self, period):
         """ base for various periods """
-        values = self.get_test_03_in()
-        values.update(holt_period=period)
-        forecast = self.forecast_obj.create(values)
-        forecast.calculate()
+        forecast = self.forecast_obj.browse(self.ref(
+            'stock_forecasting_smoothing_techniques.fst_demo_03'))
+        forecast.write({'holt_period': period})
         out = self.get_test_03_out(period)
         self.compare(out, forecast.read(out.keys())[0])
 
@@ -318,27 +206,6 @@ class TestForecasting(common.TransactionCase):
     def test_03_4(self):
         """ holt(k=4) for 11 values """
         self._test_03(4)
-
-    def get_test_03_in(self):
-        """
-        This method will return the forecast input values in a list.
-        """
-        data = {
-            'fv_01': 133,
-            'fv_02': 155,
-            'fv_03': 165,
-            'fv_04': 171,
-            'fv_05': 194,
-            'fv_06': 231,
-            'fv_07': 274,
-            'fv_08': 312,
-            'fv_09': 313,
-            'fv_10': 333,
-            'fv_11': 343,
-            'holt_alpha': 0.7,
-            'beta': 0.6,
-        }
-        return data
 
     def get_test_03_out(self, period=1):
         """
@@ -370,31 +237,10 @@ class TestForecasting(common.TransactionCase):
 
     def test_04(self):
         """ SMA(5) and WMA(5) in 12 integer values """
-        values = self.get_test_04_in()
-        forecast = self.forecast_obj.create(values)
-        forecast.calculate()
+        forecast = self.forecast_obj.browse(self.ref(
+            'stock_forecasting_smoothing_techniques.fst_demo_04'))
         out = self.get_test_04_out()
         self.compare(out, forecast.read(out.keys())[0])
-
-    def get_test_04_in(self):
-        """
-        This are data to run the forecast test_04
-        """
-        data = {
-            'fv_01': 105,
-            'fv_02': 100,
-            'fv_03': 105,
-            'fv_04': 95,
-            'fv_05': 100,
-            'fv_06': 95,
-            'fv_07': 105,
-            'fv_08': 120,
-            'fv_09': 115,
-            'fv_10': 125,
-            'fv_11': 120,
-            'fv_12': 120,
-        }
-        return data
 
     def get_test_04_out(self):
         """
@@ -408,33 +254,11 @@ class TestForecasting(common.TransactionCase):
 
     def test_05(self):
         """ SMA(3) in 12 integer values """
-        values = self.get_test_05_in()
-        forecast = self.forecast_obj.create(values)
-        forecast.calculate()
+        forecast = self.forecast_obj.browse(self.ref(
+            'stock_forecasting_smoothing_techniques.fst_demo_05'))
         self.assertEqual(forecast.period, 3)
         out = self.get_test_05_out()
         self.compare(out, forecast.read(out.keys())[0])
-
-    def get_test_05_in(self):
-        """
-        This are data to run the forecast test_05
-        """
-        data = {
-            'fv_01': 80,
-            'fv_02': 90,
-            'fv_03': 85,
-            'fv_04': 70,
-            'fv_05': 80,
-            'fv_06': 105,
-            'fv_07': 100,
-            'fv_08': 105,
-            'fv_09': 100,
-            'fv_10': 105,
-            'fv_11': 100,
-            'fv_12': 150,
-            'period': 3,
-        }
-        return data
 
     def get_test_05_out(self):
         """
