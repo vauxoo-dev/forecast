fa104b4a5c0d124d5b023367aa1b3e5ffad8b2b0
Nhomar Hernandez <nhomar@gmail.com>
2015-08-20 16:19:33 -0500

    [ADD] product_forecasting: minimal module structure

---
+++ product_forecasting/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+############################################################################
+#    Module Writen For Odoo, Open Source Management Solution
+#
+#    Copyright (c) 2011 Vauxoo - http://www.vauxoo.com
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+#    coded by: Katherine Zaoral <kathy@vauxoo.com>
+#    planned by: Nhomar Hernandez <nhomar@vauxoo.com>
+#                Gabriela Quilarque <gabriela@vauxoo.com>
+############################################################################
+
+{
+    "name": "Product relation forecasting",
+    "version": "8.0.1.0.0",
+    "author": "Vauxoo",
+    "website": "http://www.vauxoo.com/",
+    "category": "",
+    "depends": [
+        "product",
+    ],
+    "data": [
+    ],
+    "demo": [],
+    "test": [],
+    "qweb": [],
+    "js": [],
+    "css": [],
+    "installable": True,
+}
